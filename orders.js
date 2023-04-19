bcde983f561207301f0dd938866e3ec82490c690
Philip Freeman <amponsahphilip003@gmail.com>
2023-04-19 19:58:28 -0400

    Finished Project

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'95634',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber:'25661',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Drone with Camera recorder',
+        productNumber:'54361',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber:'45561',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber:'25561',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'High Tech Drone',
+        productNumber:'44321',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber:'36645',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+]
